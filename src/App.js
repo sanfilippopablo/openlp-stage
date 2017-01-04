a2322683a41c560f9ffdaf7cf6d34c67af505247
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2017-01-03 23:19:00 -0300

    Added a fix for overlapping chords

--- src/App.js
+++ src/App.js
@@ -15,7 +15,11 @@ class Line extends Component {
       <div className='line'>
         {this.props.bits.map((bit, i) => {
           let chordSpan = bit.chord ? <span className='chord'>{bit.chord}</span> : null
-          return <span className='bit' key={i}>{chordSpan}{bit.text}</span>
+          let leftMargin = 0
+          if (bit.chord !== null) {
+            leftMargin = Math.max(0, bit.chord.length - bit.text.length + 1.3)
+          }
+          return <span className='bit' key={i} style={{marginRight: leftMargin + 'em'}}>{chordSpan}{bit.text}</span>
         })}
       </div>
     )
